297fa272b6c80bb79944e3c864e64e5682b4236a
Benedikt Heine <bebe@bebehei.de>
2016-11-22 12:06:19 +0100

    [docker] add dnsmasq to log output

--- docker.sh
+++ docker.sh
@@ -6,8 +6,11 @@ export NFSHOST=${NFSHOST:-$(ip -4 addr show dev eth0 | awk '/inet/{print $2}' | 
 /srv/pixieboot/setup.sh || exit 1
 
 echo "user=root" >> $FILE_DNSMASQ_CONFIG
+echo "log-facility=/var/log/dnsmasq.log" >> $FILE_DNSMASQ_CONFIG
 
 service dnsmasq start
 service nginx start
 
-tailf /var/log/nginx/pixieboot-access.log
+tailf /var/log/nginx/pixieboot-access.log &
+tailf /var/log/dnsmasq.log &
+wait
